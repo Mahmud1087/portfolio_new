caa64d0283847f299d9e6be54ce484454af1d1c2
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-13 16:04:01 +0100

    edited the about section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,6 +8,22 @@ const AboutMe = () => {
     >
       <Title title='About Me' />
       <p className='text-lg'>
+        After graduating with a degree in{' '}
+        <span className='font-semibold'>accounting</span>, I decided to pursue
+        my passion for programming. I enrolled in a coding bootcamp and learned{' '}
+        <span className='font-semibold'>frontend web development</span>. My{' '}
+        <i>favorite part of programming</i> is the problem solving aspect. I
+        love the feeling of finally figuring out a solution to a problem. My
+        core stack is{' '}
+        <span className='font-semibold'>React, and TypeScript</span>. I am
+        currently looking for an internship or junior frontend developer roles.
+        <br />
+        <br />
+        When I'm not coding, I enjoy reading books, watching movies, and
+        watching football. I also{' '}
+        <span className='font-semibold'>enjoy learning new things</span>.
+      </p>
+      {/* <p className='text-lg'>
         Hello! I am Abdulazeez Mahmud Enesi, a passionate junior front-end
         developer and an aspiring technical writer. I hold a B. Eng. in
         Metallurgical and Materials Engineering from Ahmadu Bello University
@@ -21,7 +37,7 @@ const AboutMe = () => {
         about discussing front-end development and tech in general.I am open to
         internships, junior frontend roles, and also collaborating on web
         development projects.
-      </p>
+      </p> */}
     </div>
   );
 };
