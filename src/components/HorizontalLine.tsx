7b0aa92f3291f47fbfaa47f801f61cb48aad4093
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-15 19:19:03 +0100

    changed the text color for the title in dark mode

--- src/components/HorizontalLine.tsx
+++ src/components/HorizontalLine.tsx
@@ -1,4 +1,6 @@
 const HorizontalLine = () => {
-  return <div className='my-28 h-14 w-[2px] rounded-full bg-slate-500'></div>;
+  return (
+    <div className='my-28 h-14 w-[2px] rounded-full bg-slate-500 dark:bg-white/30'></div>
+  );
 };
 export default HorizontalLine;
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ type TitleType = {
 
 const Title = (sectionTitle: TitleType) => {
   return (
-    <h1 className='text-4xl text-gray-900 font-semibold'>
+    <h1 className='text-4xl text-gray-900 font-semibold dark:text-white dark:font-normal'>
       {sectionTitle.title}
     </h1>
   );
